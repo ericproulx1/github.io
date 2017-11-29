c72ccce614928e2afe910673298a31f7b2f08e07
ericproulx1 <33289951+ericproulx1@users.noreply.github.com>
2017-11-28 22:55:22 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -36,9 +36,9 @@ function draw() {
 
     if (keyIsDown(88) || keyIsDown(120)) { //x for brake
       me.deccelerate();
-      image(brake, window.innerWidth/4, window.innerHeight/4, 20 , 20); //make brake smaller, appear depressed
+      image(brake, Track.outerWidth+10, Track.outerHeight+10, 20 , 20); //make brake smaller, appear depressed
     } else {
-      image(brake, window.innerWidth/4, window.innerHeight/4, 20, 20);
+      image(brake, Track.outerWidth+10, Track.outerHeight+10, 20, 20);
     }
 
     if (keyIsDown(UP_ARROW) || keyIsDown(DOWN_ARROW) || keyIsDown(LEFT_ARROW) || keyIsDown(RIGHT_ARROW)) {
